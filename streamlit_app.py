38ed2f55d3c76da433c04dcc9ddb7de0403a5440
Sarvajit Khadka <104311070+sarvkk@users.noreply.github.com>
2025-03-09 18:40:11 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,27 @@ with st.expander('Data'):
   st.write('**y**')
   y=df.species
   y
+
+with st.expander('Data Visualization'):
+  st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
+
+# Data preparation
+with st.sidebar:
+  st.header('Input features')
+  island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  bill_length_mm=set.slider("Bill Length(mm)",32.1,59.6,43.9)
+  bill_depth_mm=set.slider("Bill Depth(mm)",13.1,21.5,17.2)
+  flipper_length_mm=st.slider('Flipper Length(mm)',172.0,231.0,201.0)
+  body_mass_g=st.slider("Body Mass(gm)",2700.0,4207.0,6300.0)
+  gender=st.selectbox('Gender',('Male','Female'))
+
+  #create a df for input features
+  data = {'island':island,
+          'bill_length_mm':bill_length_mm,
+          'bill_depth_mm':bill_depth_mm,
+          'flipper_length_mm':flipper_length_mm,
+          'body_mass_g':body_mass_g,
+          'gender':gender}
+  input_df= pd.DataFrame(data,index=[0])
+  input_penguins = pd.concat([input_df,X],axis=0)  
+input_df
