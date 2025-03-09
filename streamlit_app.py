e53a14974118f1956e17f512f37851dad49e4b9b
Sarvajit Khadka <104311070+sarvkk@users.noreply.github.com>
2025-03-09 19:27:24 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,8 +64,7 @@ def target_encode(val):
   return target_mapper[val]
 
 y=y_raw.apply(target_encode)
-y
-y_raw
+
 
 with st.expander('Data preparation'):
   st.write('**Encoded input penguin(X)**')
