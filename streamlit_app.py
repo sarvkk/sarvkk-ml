6970563bd9310c128374cd476660706410c17461
Sarvajit Khadka <104311070+sarvkk@users.noreply.github.com>
2025-03-09 19:41:02 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,21 +132,21 @@ st.dataframe(df_prediction_proba,
              column_config={
                  'Adelie': st.column_config.ProgressColumn(
                      'Adelie',
-                     format='%f',
+                     format='%.2f',
                      width='medium',
                      min_value=0,
                      max_value=1
                  ),
                  'Chinstrap': st.column_config.ProgressColumn(
                      'Chinstrap',
-                     format='%f',
+                     format='%.2f',
                      width='medium',
                      min_value=0,
                      max_value=1
                  ),
                  'Gentoo': st.column_config.ProgressColumn(
                      'Gentoo',
-                     format='%f',
+                     format='%.2f',
                      width='medium',
                      min_value=0,
                      max_value=1
