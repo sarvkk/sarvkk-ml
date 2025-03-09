e0a97900ec4ba8059ab2b5c561c42a09edd8ad9c
Sarvajit Khadka <104311070+sarvkk@users.noreply.github.com>
2025-03-09 19:39:16 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,7 @@ with st.sidebar:
     bill_length_mm = st.slider("Bill Length (mm)", 32.1, 59.6, 43.9)
     bill_depth_mm = st.slider("Bill Depth (mm)", 13.1, 21.5, 17.2)
     flipper_length_mm = st.slider("Flipper Length (mm)", 172.0, 231.0, 201.0)
-    # Fixed: default value set within the range (2700.0, 4207.0)
-    body_mass_g = st.slider("Body Mass (g)", 2700.0, 4207.0, 3700.0)
+    body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
     gender = st.selectbox('Gender', ('Male', 'Female'))
 
     # Create DataFrame for the input features
